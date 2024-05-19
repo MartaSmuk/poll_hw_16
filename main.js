494f221f0df9a87323f0c4c4ce7310acfed635b0
Marta Smuk <msmuk999@gmail.com>
2024-05-19 13:22:34 +0200

    changed confirm into prompt

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let sumPoints = 0;
 
 let sumNum = +prompt("How much is 2 + 2?");
-let sunEast = confirm("The sun rises in the East ?");
+let sunEast = prompt("The sun rises in the East (yes/no) ?");
 let resDivision = prompt("How much is 5 / 0 ?");
 let skyColor = prompt("What is the color of the sky?");
 let eternalQuestion = +prompt("What is the correct answer to the ultimate question of life, the universe, and everything?");
@@ -14,7 +14,7 @@ if (sumNum === 4) {
     sumPoints += 0;
 }
 
-if (sunEast === true) {
+if (sunEast === "yes") {
     sumPoints += 10;
 } else {
     sumPoints += 0;
